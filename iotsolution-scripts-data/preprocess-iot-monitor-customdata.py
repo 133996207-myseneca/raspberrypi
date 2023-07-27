1f632346e3966f4f22cc060dd6b2525ccd0a1774
133996207-myseneca <133128486+133996207-myseneca@users.noreply.github.com>
2023-07-27 13:42:38 -0400

    changes

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -202,7 +202,7 @@ latlong=lat:long'
      identifier = "IoT device performance and failures"
 
      # if dataage - use:dataage_utcoffset_timetype
-     preprocesslogic='sum, median, variance, outliers, outliersx-y, varied, anomprob'
+     preprocesslogic='sum, median, diff, avg, trend, min'
      #preprocesslogic='dataage_-4_day,trend,min,max' # millisecond,second,minute,hour,day
      #preprocesslogic='dataage_-4_hour' # millisecond,second,minute,hour,day
 #     preprocesslogic='dataage_1_minute' # millisecond,second,minute,hour,day
